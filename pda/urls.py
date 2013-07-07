b0931a7357b6f55b743199781166469500e738a1
Paul Corpuz <paul.corpuz@gmail.com>
2013-07-07 10:44:04 +0800

    Fixed: base urls.py for pda project.

--- pda/urls.py
+++ pda/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
-from notes import views
+from notepad import views
 from planner import views
 from tagger import views
 from tasks import views
 
 urlpatterns = patterns('',
-  url(r'^$', direct_to_template, {'templates':'home.html'}),
+  url(r'^$', TemplateView.as_view(template_name='home.html'), name='home_page'),
   url(r'^notepad/', include('notepad.urls')),
   url(r'^planner/', include('planner.urls')),
   url(r'^tagger/', include('tagger.urls')),
