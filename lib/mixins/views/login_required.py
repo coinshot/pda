502541b386b38dc8bf9792da590d1e16e6df50fe
Paul Corpuz <paul.corpuz@docomointertouch.com>
2013-07-17 16:30:57 +0800

    Added comments to mixins.

--- lib/mixins/views/login_required.py
+++ lib/mixins/views/login_required.py
@@ -2,6 +2,11 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
 class LoginRequiredMixin(object):
+  '''
+  Mixin for securing class-based views
+  by including the @login_required method decorator by default.
+  '''
+
   @method_decorator(login_required)
   def dispatch(self, *args, **kwargs):
     return super(LoginRequiredMixin, self).dispatch(*args, **kwargs)
--- lib/mixins/views/owned.py
+++ lib/mixins/views/owned.py
@@ -1,17 +1,28 @@
-
 class OwnedMixin(object):
+  '''
+  Mixin to ensure record/s being worked on are owned by the current user.
+  '''
 
   def get_object(self, queryset = None):
+    '''
+    Restrict retrieval of an object if user does not own the record.
+    '''
     obj = super(OwnedMixin, self).get_object()
     if obj.user != self.request.user:
       raise Http404
     return obj
 
   def get_queryset(self):
+    '''
+    Filter queryset to records owned by the current user.
+    '''
     result = super(OwnedMixin, self).get_queryset()
     result = result.filter(user = self.request.user)
     return result
 
   def form_valid(self, form):
+    '''
+    Automatically insert user ownership to record before save().
+    '''
     form.instance.user = self.request.user
     return super(OwnedMixin, self).form_valid(form)
